09fe98df534f0dc4e32f7fe7805d659a2a9d129f
oscar welton <oscarwelton@gmail.com>
2023-08-03 14:05:48 +0100

    added post model

---
+++ backend/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PostSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: Text,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 
 dotenv.config();
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the backend." });
 });
