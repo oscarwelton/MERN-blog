2d1543d410ac3171270d32c0a47d9f5eabf8612f
oscar welton <oscarwelton@gmail.com>
2023-08-03 15:06:55 +0100

    connected react app to node and express server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("http://localhost:3001/posts");
+      const data = response.data;
+      setPosts(data);
+      console.log(data);
+    })();
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="App">
       <h1>Oscar Welton Blog App</h1>
